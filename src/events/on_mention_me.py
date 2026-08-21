22663127bf9901db2d0e2b035928b1310a4c9d6a
Andrelzinnn <andrellira85@gmail.com>
2026-08-20 22:59:54 -0300

    feature: ignore dms

--- src/events/on_mention_me.py
+++ src/events/on_mention_me.py
@@ -7,14 +7,21 @@ from src.types.Env import settings
 
 @bot.event
 async def on_message(message: Message) -> None:
+
+  if not message.guild:
+    return
+
   if message.author == bot.user:
     return
+
   if message.content.lower() == "violin" or message.content.lower() == "violino":
     await message.reply(VIOLIN_GIF_URL)
     return
+
   if "yay" in message.content.lower():
     await message.reply(YAY_CAT_GIF_URL)
     return
+
   for user in message.mentions:
     if user.id == settings.user_id:
       member = message.guild.get_member(settings.user_id)
