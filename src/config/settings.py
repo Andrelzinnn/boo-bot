285dad8a4e66fe60c4e9f9b41e33cd0cfa1e5d15
Andrelzinnn <andrellira85@gmail.com>
2026-08-21 18:10:46 -0300

    fix(config): add ClassVar type annotation to model_config

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,21 +1,20 @@
 from pathlib import Path
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing_extensions import ClassVar
 
 
 class Settings(BaseSettings):
-    token: str
-    client_id: str
-    user_id: int
+    token: str = Field(default="")
+    client_id: str = Field(default="")
+    user_id: int = Field(default=0)
 
-    model_config = SettingsConfigDict(
-      env_file=Path(__file__).resolve().parents[2] / ".env",
-      env_file_encoding="utf-8",
-      extra="ignore"
+    model_config: ClassVar[SettingsConfigDict] = SettingsConfigDict(
+        env_file=Path(__file__).resolve().parents[2] / ".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
     )
 
-settings = Settings()
 
-VIOLIN_GIF_URL = "https://klipy.com/gifs/cat-instrumental-1"
-YAY_CAT_GIF_URL = "https://klipy.com/gifs/cat-chinese-4"
-GIF_URL = "https://klipy.com/gifs/cat-hello-cat-peek"
+settings = Settings()
