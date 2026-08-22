9dbeea4842c7e6932ff9839cd701e49ea8d0f956
Andrelzinnn <andrellira85@gmail.com>
2026-08-22 12:21:10 -0300

    fix(init.sh): fix virtual ambient source

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${BASE_DIR}/.dev/bin/activate"
+
+if [ -d "${BASE_DIR}/.dev" ]; then
+    source "${BASE_DIR}/.dev/bin/activate"
+elif [ -d "${BASE_DIR}/.venv" ]; then
+    source "${BASE_DIR}/.venv/bin/activate"
+else
+  echo "Erro: Nenhum Ambiente virtual encontrado" >&2
+  exit 1
+fi
+
 python3 "${BASE_DIR}/main.py"
